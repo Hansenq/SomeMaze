0fbdf6096932cea08e71785fca1c5b007bb24fbe
Hansen Qian <hansenq@fb.com>
2013-07-23 10:45:10 -0700

    Tried to add physics, but failed. Reverted back to last good state

--- SomeMaze/SMMainMaze.m
+++ SomeMaze/SMMainMaze.m
@@ -11,7 +11,9 @@
 @interface SMMainMaze ()
 
 @property BOOL contentCreated;
+@property BOOL touching;
 @property (strong, nonatomic) SKSpriteNode *actor;
+@property CGPoint lastTouch;
 
 
 @end
@@ -23,7 +25,10 @@
     /* Setup your scene here */
 
     _contentCreated = NO;
+    _touching = NO;
     _actor = nil;
+    _lastTouch.x = 0;
+    _lastTouch.y = 0;
 
     self.backgroundColor = [SKColor blackColor];
     [[UIApplication sharedApplication] setStatusBarHidden:YES];
@@ -49,26 +54,37 @@
   _actor = [SKSpriteNode spriteNodeWithColor:[UIColor redColor] size:CGSizeMake(20, 20)];
   [_actor setName:@"actor"];
   [_actor setPosition:CGPointMake(CGRectGetMidX(self.frame), 15)];
+  SKPhysicsBody *actorPhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_actor.size];
+  [actorPhysicsBody setAffectedByGravity:NO];
+  [actorPhysicsBody setAllowsRotation:NO];
+  [_actor setPhysicsBody:actorPhysicsBody];
   [self addChild:_actor];
 }
 
 - (void)addTrucks {
   NSLog(@"Making 2 Trucks!");
-  SKSpriteNode *truck = [SKSpriteNode spriteNodeWithColor:[UIColor whiteColor] size:CGSizeMake(random() % 100 + 20, 20)];
+  SKSpriteNode *truck = [SKSpriteNode spriteNodeWithColor:[UIColor grayColor] size:CGSizeMake(random() % 100 + 20, 20)];
   [truck setName:@"truck"];
   CGFloat halfTruckWidth = truck.size.width / 2;
   SKAction *action;
+//  CGFloat direction;
   if (random() % 2 == 0) {
     [truck setPosition:CGPointMake(640 + halfTruckWidth, random() % (1136 - 60) + 30)];
+//    direction = M_PI;
     action = [SKAction moveToX:0.0 - halfTruckWidth - 10 duration:2 + random() % 2];
   } else {
     [truck setPosition:CGPointMake(0.0 - halfTruckWidth, random() % (1136 - 60) + 30)];
+//    direction = 0;
     action = [SKAction moveToX:640 + halfTruckWidth + 10 duration:2 + random() % 2];
   }
   [truck runAction:[SKAction repeatActionForever:action]];
-  //    SKPhysicsBody *truckPhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:truck.size];
-  //    [truckPhysicsBody setAffectedByGravity:NO];
-  //    [truck setPhysicsBody:truckPhysicsBody];
+//  SKPhysicsBody *truckPhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:truck.size];
+//  CGFloat speed = 200;
+//  CGPoint velocity = CGPointMake(speed * cosf(direction), speed * sinf(direction));
+//  [truckPhysicsBody applyForce:velocity];
+//  [truckPhysicsBody setVelocity:velocity];
+//  [truckPhysicsBody setAffectedByGravity:NO];
+//  [truck setPhysicsBody:truckPhysicsBody];
   [self addChild:truck];
 }
 
@@ -89,22 +105,33 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
   /* Called when a touch begins */
 
-  for (UITouch *touch in touches) {
-    CGPoint location = [touch locationInNode:self];
-    if ([_actor containsPoint:location]) {
-      [_actor setPosition:location];
-    }
-
+  UITouch *touch = [touches anyObject];
+  CGPoint location = [touch locationInNode:self];
+  if ([_actor containsPoint:location]) {
+    [_actor setPosition:location];
+    _lastTouch = location;
+    _touching = YES;
+    SKPhysicsBody *actorPhysicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_actor.size];
+    [actorPhysicsBody setAffectedByGravity:NO];
+    [actorPhysicsBody setAllowsRotation:NO];
+    [_actor setPhysicsBody:actorPhysicsBody];
   }
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
 
-  for (UITouch *touch in touches) {
-    CGPoint location = [touch locationInNode:self];
+  UITouch *touch = [touches anyObject];
+  CGPoint location = [touch locationInNode:self];
+  if (_touching) {
+    CGPoint delta = CGPointMake(location.x - [self lastTouch].x, location.y - [self lastTouch].y);
     CGPoint originalPosition = [_actor position];
-    [_actor setPosition:CGPointMake(originalPosition.x, originalPosition.y + location.y)];
+    [_actor setPosition:CGPointMake(originalPosition.x + delta.x, originalPosition.y + delta.y)];
+    _lastTouch = location;
   }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+  _touching = NO;
 }
 
 
